77fb5f6632ed24f2a669694a2ee947cd4f5aff19
ilgnerbecheleni <ilgner.gui@gmail.com>
2024-03-01 11:14:59 -0300

    iniciando a aplicação

---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+ 
+ <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Estacionamento</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" to="/about">Estacionamento</RouterLink>
+        </li>
+       
+      </ul>
+    
+    </div>
+  </div>
+</nav>
+
+
+
+</template>
+
+<style scoped>
+
+</style>
